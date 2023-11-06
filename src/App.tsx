24deb76a17354fbd9aa25c28d10e9622ce936d88
Arturican <arturican@gmail.com>
2023-11-06 19:14:15 +0300

    добавил в компоненту Todolist инпут и кнопки, также же локальный стейт tasks в App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {Todolist} from "./components/Todolist";
+import {v1} from "uuid";
+
+
+export type TasksType = {
+    id: string,
+    title: string
+    isDone: boolean
+}
 
 function App() {
-  return (
-    <div className="App">
-      <Todolist/>
-    </div>
-  );
+    const tasks: TasksType[] = [
+        {id: v1(), title: 'HTML&CSS', isDone: true},
+        {id: v1(), title: 'JS', isDone: true},
+        {id: v1(), title: 'React', isDone: false},
+    ]
+    return (
+        <div className="App">
+            <Todolist
+                title={'What to learn'}
+                tasks={tasks}
+            />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import {TasksType} from "../App";
+import   './Todolist.css'
 
-export const Todolist = () => {
+export  type  TodolistType = {
+    title: string
+    tasks: TasksType[]
+}
+export const Todolist = (props: TodolistType) => {
     return (
-        <div>
-
+        <div className={'wrapper'}>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                {props.tasks.map(t =>
+                    <li key={t.id}><input type={"checkbox"} checked={t.isDone}/>{t.title}</li>)}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
         </div>
     );
 };
