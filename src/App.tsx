fe247c1d8dc4668e39bac32a197326380c06b2a5
arturican <arturican@gmail.com>
2023-11-15 16:52:56 +0300

    Добавил компоненты Rating и Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,37 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div>
+      This is App component
+      <Rating />
+      <Accordion />
+    </div>
+  );
 }
+
+const Rating = () => {
+  return (
+    <div>
+      <span>star</span>
+      <span>star</span>
+      <span>star</span>
+      <span>star</span>
+    </div>
+  );
+};
+
+const Accordion = () => {
+  return (
+    <div>
+      <h3>Меню</h3>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+};
 
 export default App;
