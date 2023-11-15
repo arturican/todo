e8be7cd245e7c57337d125dd1d34f7ebcc12e095
arturican <arturican@gmail.com>
2023-11-15 16:18:13 +0300

    подготовил ветку для пути самурая

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { Todolist } from './components/Todolist';
-import { v1 } from 'uuid';
-
-export type TasksType = {
-  id: string;
-  title: string;
-  isDone: boolean;
-};
-
-export type FilterType = 'all' | 'active' | 'completed';
 
 function App() {
-  const [tasks, setTasks] = useState<TasksType[]>([
-    { id: v1(), title: 'HTML&CSS!!!', isDone: true },
-    { id: v1(), title: 'JS', isDone: true },
-    { id: v1(), title: 'React', isDone: false },
-  ]);
-
-  let taskForTodolist = tasks;
-
-  const [filter, setFilter] = useState<FilterType>('all');
-
-  switch (filter) {
-    case 'active':
-      taskForTodolist = tasks.filter((t) => t.isDone === false);
-      break;
-    case 'completed':
-      taskForTodolist = tasks.filter((t) => t.isDone === true);
-      break;
-  }
-
-  const removeTask = (id: string) => {
-    setTasks(tasks.filter((t) => t.id !== id));
-  };
-
-  const filterTask = (filter: FilterType) => {
-    setFilter(filter);
-  };
-
-  const addTask = (title: string) => {
-    setTasks([{ id: v1(), title: title, isDone: false }, ...tasks]);
-  };
-
-  const changeCheckStaus = (isDone: boolean, id: string) => {
-    setTasks(tasks.map((t) => (t.id === id ? { ...t, isDone: !isDone } : t)));
-  };
-
-  return (
-    <div className="app">
-      <Todolist
-        title={'What to learn'}
-        tasks={taskForTodolist}
-        removeTask={removeTask}
-        filterTask={filterTask}
-        addTask={addTask}
-        changeCheckStatus={changeCheckStaus}
-        filter={filter}
-      />
-    </div>
-  );
+  return <div className="app"></div>;
 }
 
 export default App;
