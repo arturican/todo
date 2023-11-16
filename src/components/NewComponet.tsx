59392bbc5c44a11f819a1b59936e412174f9637f
arturican <arturican@gmail.com>
2023-11-16 23:02:46 +0300

    сделал задание map

--- src/components/NewComponet.tsx
+++ src/components/NewComponet.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import './style.css';
+export const NewComponet = () => {
+  const topCars = [
+    { manufacturer: 'BMW', model: 'm5cs' },
+    { manufacturer: 'Mercedes', model: 'e63s' },
+    { manufacturer: 'Audi', model: 'rs6' },
+  ];
 
-type StudentsType = {
-  id: number;
-  name: string;
-  age: number;
-};
-type NewComponetType = {
-  students: StudentsType[];
-};
-
-export const NewComponet = (props: NewComponetType) => {
   return (
     <div>
-      {props.students.map((s) => (
-        <li key={s.id}>{s.name}</li>
-      ))}
+      {topCars.map((t, index = 1) => {
+        return (
+          <table key={index} className={'table'}>
+            <th key={index}>{t.manufacturer}</th>
+            <td key={index}>{t.model}</td>
+          </table>
+        );
+      })}
     </div>
   );
 };
