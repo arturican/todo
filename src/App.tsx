a0b4df6bd30d795d87019b9466a63b57f95c1429
arturican <arturican@gmail.com>
2023-11-19 11:44:00 +0300

    подготовил файлы для повтронения todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { Todolist } from './components/Todolist';
-import { v1 } from 'uuid';
 
 export type TasksType = {
   id: string;
@@ -12,52 +10,31 @@ export type TasksType = {
 export type FilterType = 'all' | 'active' | 'completed';
 
 function App() {
-  const [tasks, setTasks] = useState<TasksType[]>([
-    { id: v1(), title: 'HTML&CSS!!!', isDone: true },
-    { id: v1(), title: 'JS', isDone: true },
-    { id: v1(), title: 'React', isDone: false },
-  ]);
-
-  let taskForTodolist = tasks;
-
-  const [filter, setFilter] = useState<FilterType>('all');
-
-  switch (filter) {
-    case 'active':
-      taskForTodolist = tasks.filter((t) => t.isDone === false);
-      break;
-    case 'completed':
-      taskForTodolist = tasks.filter((t) => t.isDone === true);
-      break;
-  }
-
-  const removeTask = (id: string) => {
-    setTasks(tasks.filter((t) => t.id !== id));
-  };
-
-  const filterTask = (filter: FilterType) => {
-    setFilter(filter);
-  };
-
-  const addTask = (title: string) => {
-    setTasks([{ id: v1(), title: title, isDone: false }, ...tasks]);
-  };
-
-  const changeCheckStaus = (isDone: boolean, id: string) => {
-    setTasks(tasks.map((t) => (t.id === id ? { ...t, isDone: !isDone } : t)));
-  };
-
   return (
     <div className="app">
-      <Todolist
-        title={'What to learn'}
-        tasks={taskForTodolist}
-        removeTask={removeTask}
-        filterTask={filterTask}
-        addTask={addTask}
-        changeCheckStatus={changeCheckStaus}
-        filter={filter}
-      />
+      <div>
+        <h3>What to learn</h3>
+        <div>
+          <input />
+          <button>+</button>
+        </div>
+        <ul>
+          <li>
+            <input type="checkbox" checked={true} /> <span>HTML&CSS</span>
+          </li>
+          <li>
+            <input type="checkbox" checked={true} /> <span>JS</span>
+          </li>
+          <li>
+            <input type="checkbox" checked={false} /> <span>React</span>
+          </li>
+        </ul>
+        <div>
+          <button>All</button>
+          <button>Active</button>
+          <button>Completed</button>
+        </div>
+      </div>
     </div>
   );
 }
