34d06746e21efc4411c9c55f9be13c0c209981f8
arturican <arturican@gmail.com>
2023-11-21 09:00:37 +0300

    подготовил для третьего урока

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Todolist } from './Todolist';
 
-export type TasksType = {
-  id: string;
-  title: string;
-  isDone: boolean;
-};
-
-export type FilterType = 'all' | 'active' | 'completed';
+export type FilterValuesType = 'all' | 'active' | 'completed';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: 'HTML&CSS', isDone: true },
+    { id: 2, title: 'JS', isDone: true },
+    { id: 3, title: 'ReactJS', isDone: false },
+    { id: 4, title: 'Rest API', isDone: false },
+    { id: 5, title: 'GraphQL', isDone: false },
+  ]);
+
+  function removeTask(id: number) {
+    const filteredTasks = tasks.filter((t) => t.id !== id);
+    setTasks(filteredTasks);
+  }
+
+  const [filter, setFilter] = useState<FilterValuesType>('all');
+
+  let tasksForTodolist = tasks;
+
+  if (filter === 'active') {
+    tasksForTodolist = tasks.filter((t) => t.isDone === false);
+  }
+  if (filter === 'completed') {
+    tasksForTodolist = tasks.filter((t) => t.isDone === true);
+  }
+
+  function changeFilter(value: FilterValuesType) {
+    setFilter(value);
+  }
+
   return (
     <div className="app">
-      <div>
-        <h3>What to learn</h3>
-        <div>
-          <input />
-          <button>+</button>
-        </div>
-        <ul>
-          <li>
-            <input type="checkbox" checked={true} /> <span>HTML&CSS</span>
-          </li>
-          <li>
-            <input type="checkbox" checked={true} /> <span>JS</span>
-          </li>
-          <li>
-            <input type="checkbox" checked={false} /> <span>React</span>
-          </li>
-        </ul>
-        <div>
-          <button>All</button>
-          <button>Active</button>
-          <button>Completed</button>
-        </div>
-      </div>
+      <Todolist title="What to learn" tasks={tasksForTodolist} removeTask={removeTask} changeFilter={changeFilter} />
     </div>
   );
 }
