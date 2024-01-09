0a10d57e7b565c327731b517b80e97473d7d09b9
arturican <arturican@gmail.com>
2024-01-09 23:10:26 +0300

    дописал Select по уроку

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Select } from './Select';
+import { useState } from 'react';
+import React from 'react';
 
 const meta: Meta<typeof Select> = {
   title: 'Select',
@@ -10,23 +12,40 @@ const meta: Meta<typeof Select> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const SelectWithValue = () => {
+  const [value, setValue] = useState('2');
+  return (
+    <Select
+      value={value}
+      onChange={setValue}
+      items={[
+        { title: 'Art', value: '1' },
+        { title: 'Dil', value: '2' },
+        { title: 'Mosk', value: '3' },
+      ]}
+    />
+  );
+};
+
+const SelectOutValue = () => {
+  const [value, setValue] = useState(null);
+  return (
+    <Select
+      value={value}
+      onChange={setValue}
+      items={[
+        { title: 'Art', value: '1' },
+        { title: 'Dil', value: '2' },
+        { title: 'Mosk', value: '3' },
+      ]}
+    />
+  );
+};
+
 export const WithValue: Story = {
-  args: {
-    items: [
-      { title: 'Art', value: '1' },
-      { title: 'Dil', value: '2' },
-      { title: 'Mosk', value: '3' },
-    ],
-    value: '2',
-  },
+  render: () => <SelectWithValue />,
 };
 
 export const WithOutValue: Story = {
-  args: {
-    items: [
-      { title: 'Art', value: '1' },
-      { title: 'Dil', value: '2' },
-      { title: 'Mosk', value: '3' },
-    ],
-  },
+  render: () => <SelectOutValue />,
 };
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,29 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, KeyboardEvent, useEffect } from 'react';
 import './Select.css';
 
 export type ItemType = {
-  value: string;
+  value: string | null;
   title: string;
 };
 
 export type SelectProps = {
-  value: string;
+  value: string | null;
   items: ItemType[];
+  onChange: (value: string | null) => void;
 };
 
 export const Select = (props: SelectProps) => {
   const [active, setActive] = useState(false);
+  const [hoveredElement, setHoveredElement] = useState(props.value);
   const selectItem = props.items.find((el) => el.value === props.value);
+  const hoveredItem = props.items.find((el) => el.value === hoveredElement);
+
+  useEffect(() => {
+    setHoveredElement(props.value);
+  }, [props.value]);
+
   const toggleItems = () => setActive(!active);
+  const onItemClick = (value: string | null) => {
+    props.onChange(value);
+    toggleItems();
+  };
+
+  const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+      for (let i = 0; i < props.items.length; i++) {
+        if (props.items[i].value === hoveredElement) {
+          const pretendentElement = e.key === 'ArrowDown' ? props.items[i + 1] : props.items[i - 1];
+          if (pretendentElement) {
+            props.onChange(pretendentElement.value);
+            return;
+          }
+        }
+      }
+      if (!selectItem) {
+        props.onChange(props.items[0].value);
+      }
+    }
+    if (e.key === 'Enter' || e.key === 'Escape') {
+      setActive(false);
+    }
+  };
   return (
-    <div>
+    <div onKeyUp={onKeyUp} tabIndex={0}>
       <span className={'main'} onClick={toggleItems}>
         {selectItem && selectItem.title}
       </span>
       {active && (
         <div className={'items'}>
           {props.items.map((el) => (
-            <div key={el.value}>{el.title}</div>
+            <div
+              key={el.value}
+              onMouseEnter={() => setHoveredElement(el.value)}
+              className={hoveredItem === el ? 'selected' : ''}
+              onClick={() => onItemClick(el.value)}
+            >
+              {el.title}
+            </div>
           ))}
         </div>
       )}
