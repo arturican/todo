4db1704fd4d42e78f3a067b78fb7c567950c946d
arturican <arturican@gmail.com>
2024-02-09 15:18:54 +0300

    создал компоненту Clock

--- src/stories/Clock.stories.tsx
+++ src/stories/Clock.stories.tsx
@@ -9,6 +9,7 @@ const Clock = () => {
 
   useEffect(() => {
     const tick = () => {
+      console.log('tik');
       const now = new Date();
       const hours = String(now.getHours()).padStart(2, '0');
       const minutes = String(now.getMinutes()).padStart(2, '0');
